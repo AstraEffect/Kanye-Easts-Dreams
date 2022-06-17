04a3554b8e5d15aad0e292818117ca15f13f9382
Ahhh_Saturn <saturnmaoudis@protonmail.com>
2022-06-17 18:11:38 +0100

    updated python data

--- processing(py)/dmHandling.py
+++ processing(py)/dmHandling.py
@@ -1,3 +1,4 @@
 from main import *
 
 print("We received the following message: " + message)
+
--- processing(py)/main.py
+++ processing(py)/main.py
@@ -1,6 +1,7 @@
 import requests
 from dmHandling import *
+message = "ping!"
 
-
-r = requests.get('http://localhost:6969/')
-message = r.text
+# r = requests.get('http://localhost:6969/')
+# message = r.text
+send = requests.post('http://localhost:4200/', data=message)
