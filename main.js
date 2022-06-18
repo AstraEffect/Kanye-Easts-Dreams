2797059f8d71bdbfa650009d3a8d7f67b0e43ea6
Ahhh_Saturn <saturnmaoudis@protonmail.com>
2022-06-18 20:59:07 +0100

    added session for cookies

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, session } = require('electron');
 const path = require('path');
 const loadMainWindow = () => {
     const mainWindow = new BrowserWindow({
